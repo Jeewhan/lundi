cb2c7318da46e129b37094948d62a6926d2f87dd
Jeewhan <distinct19@gmail.com>
2024-09-14 01:58:20 +0900

    Slack app.start
    * https://github.com/slackapi/bolt-js/blob/main/examples/getting-started-typescript/src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 import "dotenv/config";
+
+import { App } from "@slack/bolt";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start(Number(process.env.PORT) || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
