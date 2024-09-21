d80e836c04ce1d7d68f955ea148895e3138b2e23
Jeewhan <distinct19@gmail.com>
2024-09-21 13:43:03 +0900

    minor: 다른 테스트에서도 import 하게 될 mock 관련 유틸들을 utils/mock 으로 이동

---
+++ src/utils/mock.ts
@@ -0,0 +1,121 @@
+import { Faker, ko } from "@faker-js/faker";
+import {
+  clubTypes,
+  dinnerPreferredDateTimes,
+  groups,
+  keywords,
+  regions,
+} from "../constants";
+import ClubMember from "../models/club-member";
+
+export const faker = new Faker({
+  locale: [ko],
+});
+
+export const generateMockClubMemberBy = (
+  clubType: (typeof clubTypes)[number]
+): ClubMember => {
+  return new ClubMember(
+    faker.person.fullName(),
+    `U${faker.string.alphanumeric({ length: 10, casing: "upper" })}`,
+    faker.helpers.arrayElement(groups),
+    generatePhoneNumber(),
+    generateIntroduce(),
+    clubType,
+    generateGroupMembers(),
+    generateExcludedMembers(),
+    generateLunchClubKeywords(clubType),
+    generateDinnerClubLocations(clubType),
+    generateHasAppliedForLunch(clubType),
+    generateDinnerPreferredDateTime(clubType)
+  );
+};
+
+const generatePhoneNumber = () => {
+  return `010-${faker.string.numeric({
+    length: 4,
+    allowLeadingZeros: false,
+  })}-${faker.string.numeric({ length: 4, allowLeadingZeros: false })}`;
+};
+
+const generateIntroduce = (): string => {
+  const memoirNumber = faker.number.int({ min: 16, max: 99 });
+  const channelId = `C${faker.string.alphanumeric({
+    length: 10,
+    casing: "upper",
+  })}`;
+  const postId = faker.number.int({ min: 1000000000000, max: 9999999999999 });
+
+  return `https://memoir${memoirNumber}.slack.com/archives/${channelId}/p${postId}`;
+};
+
+const generateGroupMembers = (): string => {
+  return Array.from({ length: faker.number.int({ min: 0, max: 4 }) }, () =>
+    faker.person.fullName()
+  ).join(", ");
+};
+
+const generateExcludedMembers = (): string => {
+  return Array.from({ length: faker.number.int({ min: 0, max: 32 }) }, () =>
+    faker.person.fullName()
+  ).join(", ");
+};
+
+const generateLunchClubKeywords = (
+  clubType: (typeof clubTypes)[number]
+): string => {
+  if (clubType === "dinner") {
+    return "";
+  }
+
+  const selectedKeywords = faker.helpers.arrayElements(keywords, {
+    min: 1,
+    max: keywords.length,
+  });
+
+  return selectedKeywords.join(", ");
+};
+
+const generateDinnerClubLocations = (
+  clubType: (typeof clubTypes)[number]
+): string => {
+  if (clubType === "lunch") {
+    return "";
+  }
+
+  return faker.helpers
+    .arrayElements(regions, {
+      min: 1,
+      max: regions.length,
+    })
+    .join(", ");
+};
+
+const generateHasAppliedForLunch = (
+  clubType: (typeof clubTypes)[number]
+): boolean => {
+  if (clubType === "dinner") {
+    return false;
+  }
+
+  return faker.number.int({ min: 0, max: 100 }) > 30;
+};
+
+const generateDinnerPreferredDateTime = (
+  clubType: (typeof clubTypes)[number]
+): string => {
+  if (clubType === "lunch") {
+    return "";
+  }
+
+  if (faker.number.int({ min: 0, max: 100 }) < 30) {
+    return "";
+  }
+
+  const selectedDates = faker.helpers.arrayElements(dinnerPreferredDateTimes, {
+    min: 1,
+    max: 4,
+  });
+
+  return selectedDates.join(", ");
+};
