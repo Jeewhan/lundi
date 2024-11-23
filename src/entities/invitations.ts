aaa8d56a178a2e7da338d14bd24dbf16a96d2326
Jeewhan <distinct19@gmail.com>
2024-11-23 18:54:56 +0900

    🚧 문구수정

--- src/entities/invitations.ts
+++ src/entities/invitations.ts
@@ -36,70 +36,49 @@ const LUNCH_DINNER_CLUB_INVITE_BUTTON_LAYOUT = [
     type: "section",
     text: {
       type: "plain_text",
-      text: "정기모임을 넘어 더 다양한 분들과 만나고 싶으실 경우,\n새로운 멤버들과 만나 같이 밥 먹으며 네트워킹해요!\n각양각색 메모어 멤버들과의 만남 속, 깊고 다채로운 이야기가 기다리고 있어요.",
+      text: "정기모임을 넘어 더 다양한 분들과 만나고 싶으실 경우,\n새로운 멤버들과 만나 같이 밥 먹으며 네트워킹해요!\n다양한 메모어 멤버들과의 만남 속, 깊고 다채로운 이야기가 기다리고 있어요.",
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: "*런치클럽*\n런치클럽은 `일대일`로 만나 식사하며 *관심사 기반으로 대화를 나눠요!*\n자세한 운영 내용은 <https://www.notion.so/e75efa945f7c41a0b7898bd934cc8c38|여기>를 참고해주세요!\n> 꼭 점심에 만나야될 필요는 없어요! 매칭된 상대방과 일정을 조율하여 만나는 방식입니다.",
+      text: ":pizza: *런치클럽*\n*일대일* 로 만나 식사하며 *관심사 기반으로 대화를 나눠요!*\n> 자세한 운영 내용은 <https://www.notion.so/e75efa945f7c41a0b7898bd934cc8c38|여기>를 참고해주세요!\n> 꼭 점심에 만나야될 필요는 없어요!\n> 매칭된 상대방과 일정을 조율하여 만나는 방식입니다.",
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: "*디너클럽*\n디너클럽은 `3~5인` 그룹으로 만나 맛있는 *저녁식사를 함께 즐겨요*!\n자세한 운영 내용은 <https://www.notion.so/97ddad22b2c141b79fdff96b8d9ba444|여기>를 참고해주세요!\n> 신청 할 때 일정과 지역을 모두 미리 결정 후 만나요. 같은 일정을 선택한 분들과 매칭돼요.",
+      text: ":knife_fork_plate: *디너클럽*\n*3~5인* 그룹으로 만나 맛있는 *저녁식사를 함께 즐겨요*!\n> 자세한 운영 내용은 <https://www.notion.so/97ddad22b2c141b79fdff96b8d9ba444|여기>를 참고해주세요!\n> 신청할 때 일정과 지역을 모두 미리 결정 후 만나요.\n> 같은 일정과 지역을 선택한 분들과 매칭돼요.",
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: "`런치/디너 하나만 참여할 수도, 둘 다 참여할 수도 있어요.`",
+      text: "이번 17기는 각각 한 번의 매칭을 진행해요.",
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: "`이번 17기는 각각 한 번의 매칭을 진행해요.`",
+      text: "• *신청기한*: 다음 주 화요일(11/26)까지\n• *최종매칭*: 다음 주 금요일(11/29) 중\n• *매칭방식*: 슬랙 그룹메시지 개설\n• *참여방법*: 아래 '참가신청'을 눌러 매칭에 필요한 내용을 작성해 주세요.\n• *수정방법*: 만약 수정을 원하실 경우, 재신청 해주세요. 가장 최근 제출된 신청 내역으로 매칭해 드려요.\n• <https://ssgi.notion.site/FAQ-14663dfa4a5080a19037caa8a85232da|런치/디너 클럽 FAQ>",
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: ":spiral_calendar_pad: *신청 기한*: 다음 주 화요일(11/26)까지",
+      text: `:bangbang: *런치/디너 클럽에 참여하려면 <#${process.env.MEMOIR_17_INTRODUCE_CHANNEL}> 채널에 소개 작성이 꼭 필요합니다.*`,
     },
   },
   {
     type: "section",
     text: {
       type: "mrkdwn",
-      text: ":pizza: *최종 매칭*: 다음 주 금요일(11/29) 중",
-    },
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: ":speech_balloon: *매칭 방식*: 슬랙 그룹메시지 개설",
-    },
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: ":white_check_mark: *참여 방법*: 아래 '참가신청'을 눌러 매칭에 필요한 내용을 작성해 주세요.",
-    },
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: `:bangbang: 런치/디너 클럽에 참여하려면 <#${process.env.MEMOIR_17_INTRODUCE_CHANNEL}> 채널에 *소개 작성이 꼭 필요합니다.*`,
+      text: "*신청버튼을 누르고 10초가 지나도 창이 열리지 않을 경우, 버튼을 다시 눌러주세요.*",
     },
   },
   {
--- src/views/club-join-modal-view.ts
+++ src/views/club-join-modal-view.ts
@@ -27,7 +27,7 @@ import {
 
 export class ClubJoinModalView {
   constructor(
-    private readonly user?: UserDTO, // private readonly existingJoin?: ClubJoinRecordDTO,
+    private readonly user: UserDTO, // private readonly existingJoin?: ClubJoinRecordDTO,
   ) {}
 
   public joinLunchClub(
@@ -39,9 +39,12 @@ export class ClubJoinModalView {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "런치클럽을 선택해주셨어요!\n모임 배정을 위해 아래 내용을 선택해주세요.",
+          text: "런치클럽을 선택해주셨어요!\n매칭을 위해 아래 내용을 선택해주세요.",
         },
       },
+
+      ...this.spacerBlock,
+
       ...this.getLunchClubTopicSelectionBlocks(),
     ]);
   }
@@ -55,9 +58,12 @@ export class ClubJoinModalView {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "디너클럽을 선택해주셨어요!\n모임 배정을 위해 아래 내용을 선택해주세요.",
+          text: "디너클럽을 선택해주셨어요!\n매칭을 위해 아래 내용을 선택해주세요.",
         },
       },
+
+      ...this.spacerBlock,
+
       ...this.getDinnerClubTopicSelectionBlocks(),
     ]);
   }
@@ -71,10 +77,16 @@ export class ClubJoinModalView {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "런치클럽 & 디너클럽을 동시에 선택해주셨어요!\n모임 배정을 위해 아래 내용을 선택해주세요.",
+          text: "런치클럽 & 디너클럽을 동시에 선택해주셨어요!\n매칭을 위해 아래 내용을 선택해주세요.",
         },
       },
+
+      ...this.spacerBlock,
+
       ...this.getLunchClubTopicSelectionBlocks(),
+
+      ...this.spacerBlock,
+
       ...this.getDinnerClubTopicSelectionBlocks(),
     ]);
   }
@@ -82,8 +94,28 @@ export class ClubJoinModalView {
   private get excludedMembers(): string[] {
     return (
       // this.existingJoin?.[만나지_않아도_될_멤버들] ??
-      this.user?.[만나지_않아도_될_멤버들] ?? []
+      this.user[만나지_않아도_될_멤버들] ?? []
     );
+  }
+
+  private get spacerBlock(): KnownBlock[] {
+    return [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: " ",
+        },
+      },
+
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: " ",
+        },
+      },
+    ];
   }
 
   // private get dinnerDatetimes(): MrkdwnOption[] {
@@ -136,7 +168,7 @@ export class ClubJoinModalView {
     title: string,
     blocks: KnownBlock[],
   ): ViewsOpenArguments {
-    if (!this.user?.isValidIntroduce) {
+    if (!this.user.isValidIntroduce) {
       return this.getNotReadyForJoinClubView(body, payload, title);
     }
 
@@ -169,22 +201,18 @@ export class ClubJoinModalView {
 
           this.getPhoneNumberInputBlock(),
 
-          {
-            type: "divider",
-          },
+          ...this.spacerBlock,
 
           ...blocks,
 
-          {
-            type: "divider",
-          },
+          ...this.spacerBlock,
 
           ...this.getExcludedMembersSelectionBlocks(),
 
+          ...this.spacerBlock,
+
           // ...(this.existingJoin
-          ...(false
-            ? []
-            : [{ type: "divider" }, ...this.getPolicyAgreementBlocks()]),
+          ...(false ? [] : this.getPolicyAgreementBlocks()),
         ],
       },
     };
@@ -230,27 +258,25 @@ export class ClubJoinModalView {
     };
   }
 
-  private getNameVerificationBlocks() {
+  private getNameVerificationBlocks(): KnownBlock[] {
     return [
-      ...(this.user
-        ? [
-            {
-              type: "section",
-              text: {
-                type: "mrkdwn",
-                text: `*${this.user[성함]}님* 참가신청을 진행합니다.`,
-              },
-            },
-          ]
-        : []),
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*${this.user[성함]}님* 참가신청을 진행합니다.`,
+        },
+      },
 
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "만약 위에 적힌 성함이 실명 전체 또는 '홍길동A' 등이 아닌, 영문 또는 별명 그리고 성함의 일부(성 없이 이름) 등으로 나와있으실 경우, 원활한 운영을 위해 슬랙에서 *본명으로 꼭 변경해주신 뒤에 신청해주시길 부탁드려요.*",
+          text: "*[필수]* 위에 적힌 성함이 영문, 별명, 성함의 일부(ex. 성 없이 이름) 등으로 나와있으실 경우, 원활한 운영을 위해 슬랙에서 전체 실명(알파벳 포함 | 홍길동A, 김철수B 등)으로 *변경해주신 뒤에 신청해주세요.*",
         },
       },
+
+      ...this.spacerBlock,
     ];
   }
 
@@ -262,7 +288,8 @@ export class ClubJoinModalView {
 
       label: {
         type: "plain_text",
-        text: "원활한 모임 진행을 위해 핸드폰 번호를 받습니다.\n(연락처는 매칭된 그룹 내에서만 공유됩니다!)",
+        text: ":iphone: 원활한 매칭 진행을 위해 핸드폰 번호를 받습니다.\n(연락처는 매칭된 그룹 내에서만 공유됩니다!)",
+        emoji: true,
       },
 
       element: {
@@ -287,7 +314,7 @@ export class ClubJoinModalView {
 
         text: {
           type: "mrkdwn",
-          text: "만나지 않아도 되는 멤버가 있다면, 매칭에서 제외할 수 있습니다 :)\n- 정확하게 선택해주셔야만 제대로 반영될 수 있습니다.\n- 같은 회고모임에 배정되어있는 분들은 기본적으로 포함되었습니다.",
+          text: ":one: 만나지 않아도 되는 멤버가 있다면, 매칭에서 제외할 수 있습니다.\n- 같은 회고모임 멤버들은 기본적으로 포함되어있습니다.",
         },
       },
 
@@ -328,7 +355,7 @@ export class ClubJoinModalView {
 
         text: {
           type: "mrkdwn",
-          text: "노쇼 방지를 위한 보증금 제도가 있습니다. 그룹 내에서 모임 일정 확정 후 불참하실 경우, 메모어 보증금에서 1만원이 차감됩니다. (예: 1회 불참 시 1만원, 3회 불참 시 3만원 차감)",
+          text: ":two: 노쇼 방지를 위한 보증금 제도가 있습니다. 그룹 내에서 일정 확정 후 불참하실 경우, 메모어 보증금에서 1만원이 차감됩니다. (예: 1회 불참 시 1만원, 3회 불참 시 3만원 차감)",
         },
       },
 
@@ -356,17 +383,19 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "일정이 확정되면 꼭 참여해주세요!\n혹시나 일정변경이 필요하다면 바로 공유해주세요.",
+          text: "부득이한 사유로 일정변경이 필요하다면 바로 매칭된 분들에게 공유해주세요.",
           emoji: true,
         },
       },
+
+      ...this.spacerBlock,
 
       {
         type: "section",
 
         text: {
           type: "mrkdwn",
-          text: "런치/디너 클럽 매칭 과정에서 매칭 인원의 부족, 선호 및 관심 요소에 대한 부족으로 해당 회차에 한해 매칭이 안될 수도 있습니다.\n자주 발생하는 일은 아니며, 매칭이 될 수 있도록 최선을 다하겠습니다.",
+          text: ":three: 매칭 과정에서 매칭 인원의 부족, 선호 및 관심 요소에 대한 부족으로 해당 회차에 한해 매칭이 안될 수도 있습니다.\n자주 발생하는 일은 아니며, 매칭이 될 수 있도록 최선을 다하겠습니다.",
         },
       },
 
@@ -399,12 +428,14 @@ export class ClubJoinModalView {
         },
       },
 
+      ...this.spacerBlock,
+
       {
         type: "section",
 
         text: {
           type: "mrkdwn",
-          text: "메모어 런치/디너는 CS 응대가 어렵습니다.\n대신, 지난 2년간 쌓아온 데이터로 상황에 따른 FAQ 메뉴얼을 만들어 공유드릴 예정이에요.",
+          text: ":four: 메모어 런치/디너는 CS 응대가 어렵습니다.",
         },
       },
 
@@ -432,17 +463,19 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "너른 양해 부탁드립니다.",
+          text: "모집글의 FAQ를 참고해주세요.",
           emoji: true,
         },
       },
+
+      ...this.spacerBlock,
 
       {
         type: "section",
 
         text: {
           type: "mrkdwn",
-          text: "*[개인정보 수집·이용 및 제3자 제공 동의서]*\n본인은 다음과 같이 메모어 17기 신청 정보 중 일부를 본 동의서에서 정하는 경우에 한하여 메모어 런치/디너 클럽장에게 제공하는 것을 동의합니다.\n1. 개인정보 제3자 제공 동의\n- 제공받는 자: 런치/디너 클럽장\n2. 제공받는 자의 이용 목적\n- 런치/디너 클럽의 같은 모임원 혹은 중복 매칭 방지\n- 런치/디너 클럽의 모임방 생성 자동화\n- 모임 배정 고도화\n3. 제공 항목 : 성함, 연락처, 소속 모임명\n4. 제공받는 자의 보유ㆍ이용 기간\n- 17기 런치/디너 클럽 진행 기간동안 보관하며, 운영 종료 시 파기\n동의 거부권리 : 위 개인정보 제공 동의를 거부하실 수 있음. 다만 이 경우 런치/디너 클럽의 가입 및 프로그램 이용이 거부될 수 있습니다.",
+          text: ":five: 개인정보 수집·이용 및 제3자 제공 동의서\n본인은 다음과 같이 메모어 17기 신청 정보 중 일부를 본 동의서에서 정하는 경우에 한하여 메모어 런치/디너 클럽장에게 제공하는 것을 동의합니다.\n1. 개인정보 제3자 제공 동의\n- 제공받는 자: 런치/디너 클럽장\n2. 제공받는 자의 이용 목적\n- 런치/디너 클럽의 같은 모임원 혹은 중복 매칭 방지\n- 런치/디너 클럽의 모임방 생성 자동화\n- 모임 배정 고도화\n3. 제공 항목 : 성함, 연락처, 소속 모임명\n4. 제공받는 자의 보유ㆍ이용 기간\n- 17기 런치/디너 클럽 진행 기간동안 보관하며, 운영 종료 시 파기\n동의 거부권리 : 위 개인정보 제공 동의를 거부하실 수 있음. 다만 이 경우 런치/디너 클럽의 가입 및 프로그램 이용이 거부될 수 있습니다.",
         },
       },
 
@@ -470,7 +503,7 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "클럽 운영을 위한 개인정보 수집·이용 및 제3자 제공 동의에 관한 항목입니다.\n개인정보 제3자 제공에 동의하실까요?",
+          text: "개인정보 제3자 제공에 동의하실까요?",
           emoji: true,
         },
       },
@@ -536,7 +569,7 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "런치클럽에서 만나 나누고 싶은 대화 주제(관심사)를 골라주세요!\n복수 선택이 가능해요. 많이 고를수록 더 다양한 만남이 가능해요.",
+          text: ":pizza: 런치클럽에서 만나 나누고 싶은 대화 주제(관심사)를 골라주세요!\n복수 선택이 가능해요. 많이 고를수록 더 다양한 만남이 가능해요.",
           emoji: true,
         },
       },
@@ -595,10 +628,12 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "디너클럽 참석이 가능한 일정을 모두 선택해주세요. 선택 후 변경은 어려워요.(복수선택 가능)\n참여 일정 5개 체크 시 : 0~5번의 매칭 가능\n참여 일정 3개 체크 시: 0~3번의 매칭 가능\n참여 일정 1개 체크 시 : 0~1번의 매칭 가능\n- 선택일을 적게 할 경우 매칭이 안될 수 있어요.",
+          text: ":knife_fork_plate: 디너클럽 참석이 가능한 일정을 모두 선택해주세요. 선택 후 변경은 어려워요.(복수선택 가능)\n참여 일정 5개 체크 시 : 0~5번의 매칭 가능\n참여 일정 3개 체크 시: 0~3번의 매칭 가능\n참여 일정 1개 체크 시 : 0~1번의 매칭 가능\n- 선택일을 적게 할 경우 매칭이 안될 수 있어요.",
           emoji: true,
         },
       },
+
+      ...this.spacerBlock,
 
       {
         type: "input",
@@ -647,7 +682,7 @@ export class ClubJoinModalView {
 
         label: {
           type: "plain_text",
-          text: "오프라인 모임을 위해 선호하는 지역을 골라주세요.(복수선택 가능)\n- 많이 고를수록 더 다양한 만남이 가능해요.",
+          text: ":knife_fork_plate: 디너클럽 모임을 위해 선호하는 지역을 골라주세요.(복수선택 가능)\n- 많이 고를수록 더 다양한 만남이 가능해요.",
           emoji: true,
         },
       },
